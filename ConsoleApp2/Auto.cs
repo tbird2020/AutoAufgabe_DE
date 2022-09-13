71eecc8c0cc74e7e14cefe501bb8d776a75c4cec
tbird <tbird@DESKTOP-HK09J9K>
2022-09-13 14:24:56 +0200

    Add project files.

---
+++ ConsoleApp2/Auto.cs
@@ -0,0 +1,147 @@
+﻿namespace Klassenaufgabe
+{
+    public class Auto
+    {
+        //readonly, kommt nur vom konstruktor und soll und darf danach nicht mehr veraendert werden.
+        readonly float _maxKraftstoffmenge;
+        readonly float _verbrauch;
+
+        //(private) klassenvariablen, mit denen gearbeitet wird.
+        int _kilometerstand;
+        float _kraftstoffmenge;
+        bool _fahrbereit;
+
+        /// <summary>
+        /// Default Konstruktor. Erstellt ein neues Auto.
+        /// </summary>
+        public Auto()
+        {
+        }
+
+        /// <summary>
+        /// Erstellt ein neues Auto mit vorgegebenem Tankvolumen (mit vollem Tank) und vorgegebenem Verbrauch
+        /// </summary>
+        /// <param name="maxKraftstoffmenge">Kraftstoffmenge, welche in das Auto initial getankt werden soll.</param>
+        /// <param name="verbrauch">Verbrauch des Autos je 10km</param>
+        public Auto(int maxKraftstoffmenge, int verbrauch)
+        {
+            //neues auto -> 0 km.
+            _kilometerstand = 0;
+            _maxKraftstoffmenge = maxKraftstoffmenge;
+
+            //annahme: tank voll bei erstellen des autos.
+            _kraftstoffmenge = _maxKraftstoffmenge;
+            _verbrauch = verbrauch;
+
+            //fahrbereit ist das auto nur, wenn verbrauch und maxKraftstoffmenge auf werte >0 gesetzt sind.
+            _fahrbereit = maxKraftstoffmenge > 0 && verbrauch > 0;
+        }
+
+        /// <summary>
+        /// Fahren. Macht alles was es braucht, damit ein Auto faehrt.
+        /// </summary>
+        /// <param name="distanz">Distanz, welche gefahren werden soll.</param>
+        public void Fahre(int distanz)
+        {
+            Console.WriteLine($"Das Auto soll {distanz} km fahren.");
+
+            //initiale pruefung. kann das auto ueberhaupt fahren (tank nicht leer etc?)
+            if (!_fahrbereit)
+            {
+                Console.WriteLine("Auto ist nicht fahrbereit!");
+                return;
+            }
+
+            //lokale variable zum merken, wie viel km der distanz schon gefahren wurden.
+            int gefahreneKm = 0;
+
+            //schleife, welche die distanz farhrt. intervall wie vorgegeben
+            while (gefahreneKm < distanz)
+            {
+                //wie viele km sind wir von der distanz schon gefahren?
+                gefahreneKm++;
+
+                //gesamt-km stand erhoehen
+                _kilometerstand++;
+
+                //alle 10km wird die Kraftstoffmenge um den Verbrauch des Autos reduziert (unt ein paar tests durchgefuehrt)
+                if (_kilometerstand % 10 == 0)
+                {
+                    _kraftstoffmenge -= _verbrauch;
+
+                    //falls mal negative tankvolumen auftreten sollten ...
+                    if (_kraftstoffmenge < 0) _kraftstoffmenge = 0;
+
+                    //wenn nur noch 1/6 der maximalen menge sprit im tank ist, soll eine warnung ausgegeben werden
+                    if (_kraftstoffmenge <= _maxKraftstoffmenge / 6 && _kraftstoffmenge > 0)
+                    {
+                        Console.WriteLine($"ACHTUNG! Nicht mehr viel Sprit im Tank! ({_kraftstoffmenge} liter)");
+                    }
+
+                    //wenn tank leer, dann nix mehr fahren. 
+                    if (_kraftstoffmenge <= 0)
+                    {
+                        _fahrbereit = false;
+                        Console.WriteLine($"Sprit leer nach {gefahreneKm} km.");
+                        break;
+                    }
+                }
+
+                //automatischer statusbericht alle 150km.
+                if (_kilometerstand % 150 == 0)
+                {
+                    Console.Write("Automatischer ");
+                    ErstelleStatusbericht();
+                }
+
+                //bei mehr als 1500km ist der motor kaputt. Das auto ist dann nicht mehr fahrbereit.
+                if (_kilometerstand >= 1500)
+                {
+                    Console.WriteLine("Motor kaputt.");
+                    _fahrbereit = false;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Das Auto ist {gefahreneKm} km gefahren.");
+            Console.WriteLine($"Das Auto hat jetzt einen Kilometerstand von {_kilometerstand} km.");
+        }
+
+        /// <summary>
+        /// Erstellt einen Statusbericht
+        /// </summary>
+        public void ErstelleStatusbericht()
+        {
+            //einfacher statusbericht. kann wohl noch erweitert werden?
+            Console.WriteLine($"Statusbericht: Das Auto hat einen Kilometerstand von {_kilometerstand} km und es sind {_kraftstoffmenge} liter im Tank. Es ist{(_fahrbereit ? "" : " nicht mehr")} fahrbereit.");
+        }
+
+        /// <summary>
+        /// Tankt das Auto voll. 
+        /// </summary>
+        public void Tanken()
+        {
+            Console.WriteLine($"Es werden {_maxKraftstoffmenge - _kraftstoffmenge}l getankt.");
+
+            //der einfachheit halber wird die kraftstoffmenge auf die maximal moegliche menge gesetzt.
+            _kraftstoffmenge = _maxKraftstoffmenge;
+
+            Console.WriteLine($"Es sind jetzt wieder {_kraftstoffmenge} liter im Tank.");
+
+            //das auto ist fahrbereit, wenn sprit im tank ist.
+            _fahrbereit = _kraftstoffmenge > 0;
+        }
+
+        /// <summary>
+        /// Tauscht den Motor des Autos. Neuer Motor = Kilometerstand 0.
+        /// </summary>
+        public void TauscheMotor()
+        {
+            Console.WriteLine("Neuer Motor wird eingebaut. Der Kilometerstand wird daher zurueckgesetzt.");
+
+            //neuer motor -> das auto ist wieder fahrbereit. der kilometerstand des neuen motors betraegt 0km.
+            _fahrbereit = true;
+            _kilometerstand = 0;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Klassenaufgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Erstelle erstes Auto");
+
+            //erstes auto, wird mit 50l getankt und hat einen verbrauch von 4l / 10km.
+            var erstesAuto = new Auto(50, 4);
+            erstesAuto.Fahre(10);
+            erstesAuto.Fahre(30);
+            erstesAuto.ErstelleStatusbericht();
+            erstesAuto.Fahre(50);
+            erstesAuto.Tanken();
+            erstesAuto.Fahre(100);
+            erstesAuto.Fahre(400);
+            erstesAuto.ErstelleStatusbericht();
+            erstesAuto.Fahre(30);
+            erstesAuto.Fahre(600);
+            erstesAuto.Tanken();
+            erstesAuto.ErstelleStatusbericht();
+            erstesAuto.Fahre(300);
+            erstesAuto.ErstelleStatusbericht();
+
+            Console.WriteLine("\n\nErstelle zweites Auto");
+
+            //zweites auto. erstellung durch default constructor.
+            var zweitesAuto = new Auto();
+            zweitesAuto.Fahre(10);
+            zweitesAuto.Fahre(30);
+            zweitesAuto.ErstelleStatusbericht();
+            zweitesAuto.Fahre(50);
+            zweitesAuto.Tanken();
+            zweitesAuto.Fahre(100);
+            zweitesAuto.Fahre(400);
+            zweitesAuto.ErstelleStatusbericht();
+            zweitesAuto.Fahre(30);
+            zweitesAuto.Fahre(600);
+            zweitesAuto.Tanken();
+            zweitesAuto.ErstelleStatusbericht();
+            zweitesAuto.Fahre(300);
+            zweitesAuto.ErstelleStatusbericht();
+        }
+    }
+}
